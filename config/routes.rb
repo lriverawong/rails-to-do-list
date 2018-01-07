438952300d685c733fdd9d05bd575f591a030c9c
lriverawong <lriverawong@outlook.com>
2018-01-07 02:10:49 -0500

    mark as completed finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :todo_lists do
     resources :todo_items do
-      patch :complete
+      member do
+        patch :complete
+      end
     end
   end
   root 'todo_lists#index'
